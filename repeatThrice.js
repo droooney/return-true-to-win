fe1aa51a6563a7b64503660432e0d87fafbe9c0f
droooney <d.vor7@mail.ru>
2018-11-25 21:10:22 +0500

    add repeatThrice

---
+++ repeatThrice.js
@@ -0,0 +1,14 @@
+// submitted by droooney
+
+verifyInput = (input) => {
+  const parsed = JSON.parse(input);
+  let wrong;
+
+  if (Array.from(new Set(parsed)).some(c => [...parsed].filter(x => x === (wrong = c)).length !== 3)) {
+    throw `All characters must be repeated exactly three times (${JSON.stringify(wrong)} is not)`;
+  }
+};
+
+function repeatThrice(x) {
+  return eval(x) === 'hello world!';
+}
