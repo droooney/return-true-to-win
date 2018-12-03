4d48a99783169417570304bc9df69f791e5ccb80
droooney <d.vor7@mail.ru>
2018-12-03 22:25:36 +0500

    add dozen9

---
+++ dozen9.js
@@ -0,0 +1,9 @@
+// submitted by droooney
+
+verifyInput = (input) => {
+  if (new Set(input).size > 12) throw 'Dozen characters now';
+};
+
+function dozen9(x) {
+  return x === 'javascript is very much fun';
+}
