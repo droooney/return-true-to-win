5375e28f033db6ef23dc35bb1552d5abbe6f01b2
droooney <d.vor7@mail.ru>
2018-12-06 21:14:06 +0500

    add dozen2

---
+++ dozen2.js
@@ -0,0 +1,14 @@
+// submitted by droooney
+
+verifyInput = (input) => {
+  if (new Set(input).size > 12) throw 'Dozen characters now';
+};
+
+function dozen2(palindrome) {
+  return (
+    typeof palindrome === 'string'
+    && palindrome.length > 1e3
+    && new Set(palindrome).size > 12
+    && palindrome === [...palindrome].reverse().join('')
+  );
+}
