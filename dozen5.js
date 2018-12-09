4513829253a5c585a067785620d139c63dc7fec5
droooney <d.vor7@mail.ru>
2018-12-09 22:51:41 +0500

    change rands to objects

--- dozen5.js
+++ dozen5.js
@@ -7,21 +7,21 @@ verifyInput = (input) => {
 };
 
 const size = 10;
-const getRandomInt = (upperBound = 1e3) => new Number(Math.floor(Math.random() * upperBound));
+const getRandomIndex = () => Math.floor(Math.random() * size);
 const values = [...Array(1e3)].map(() => {
-  const rands = [...Array(size)].map(getRandomInt);
-  const firstRand = rands[0];
-  const swap = (i, j) => [rands[i], rands[j]] = [rands[j], rands[i]];
+  const objects = [...Array(size)].map(() => ({}));
+  const firstObj = objects[0];
+  const swap = (i, j) => [objects[i], objects[j]] = [objects[j], objects[i]];
   const swaps = [];
 
   for (let i = 0; i < 10; i++) {
-    const [i, j] = [+getRandomInt(size), +getRandomInt(size)];
+    const [i, j] = [0, 0].map(getRandomIndex);
 
     swaps.push([i, j]);
     swap(i, j);
   }
 
-  return [rands.indexOf(firstRand), swaps];
+  return [objects.indexOf(firstObj), swaps];
 });
 
 function dozen5(f) {
